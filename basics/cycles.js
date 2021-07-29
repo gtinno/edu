9681b5411a68ddb3a656a35f3d8401cf54c12f3a
gtinno <gennady@innoflow.io>
2021-07-29 14:41:12 +0300

    mods

--- basics/cycles.js
+++ basics/cycles.js
@@ -1,6 +1,21 @@
-for (let i=1; i <= 5; i++)
+for (let i=1; i <= 2; i++)
 {
     console.log("i=",i)
+}
+
+let i = 0;
+do
+{
+    console.log("i=",i)
+    i++;
+}
+while (i < 2)
+
+i = 0;
+while (i < 2)
+{
+    console.log("i=",i)
+    i++;  
 }
 
 
