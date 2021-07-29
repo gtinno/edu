d6daab63319c9a5c6bfe5f0939d251e3ccb61c9d
gtinno <gennady@innoflow.io>
2021-07-29 12:17:35 +0300

    initial commit

---
+++ basics/arrays.js
@@ -0,0 +1,13 @@
+let a = 0
+let b = 1
+
+const list = ['a','b','c']
+const list2 = Array.of (1,2,3)
+
+//list[0] = 'a'
+
+let c = a + b
+// console.log(list2)
+// console.log(list2[1])
+
+list.forEach(el => console.log(el))
---
+++ basics/functions.js
@@ -0,0 +1,20 @@
+function doSomething(a,b)
+{
+    return a + b
+}
+
+const doSomethingAgain = function(a,b)
+{
+    return a -b
+}
+
+console.log(doSomething(1,2))
+console.log(doSomethingAgain(1,2))
+
+const myFunction = a => {
+    a / 2
+} 
+
+let a = myFunction(8);
+
+//console.log(a)
